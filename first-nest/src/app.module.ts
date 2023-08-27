d46585d97b9f7ef674e6f2c58d94fe6d4e84f12a
Nawodya Ishan <nawodyain@gmail.com>
2023-08-27 17:38:57 +0530

    feat: prettier

--- first-nest/src/app.module.ts
+++ first-nest/src/app.module.ts
@@ -1,19 +1,18 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-    // The `imports` array is used to import other modules.
-    // Leave empty for this example as there are no additional modules being imported.
-    imports: [],
+  // The `imports` array is used to import other modules.
+  // Leave empty for this example as there are no additional modules being imported.
+  imports: [],
 
-    // The `controllers` array specifies the controllers to be registered within this module.
-    // Controllers handle incoming HTTP requests and define the API endpoints.
-    controllers: [AppController],
+  // The `controllers` array specifies the controllers to be registered within this module.
+  // Controllers handle incoming HTTP requests and define the API endpoints.
+  controllers: [AppController],
 
-    // The `providers` array defines the services that can be injected into other components.
-    // Services encapsulate the business logic of the application.
-    providers: [AppService],
+  // The `providers` array defines the services that can be injected into other components.
+  // Services encapsulate the business logic of the application.
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
