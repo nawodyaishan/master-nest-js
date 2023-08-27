2e9bf3e6eefa4c913603ee5e298f2d12406968a3
Nawodya Ishan <nawodyain@gmail.com>
2023-08-27 17:37:29 +0530

    feat: add nest project

---
+++ first-nest/src/main.ts
@@ -0,0 +1,10 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+// Creation New Nest Application Object
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+
+bootstrap();
