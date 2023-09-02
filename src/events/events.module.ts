851d0535dd08a6533312d4aa9328eb2473d5127b
nawodyaishan <nawodyain@gmail.com>
2023-09-02 22:47:14 +0530

    feat: update with docker yaml

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,20 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
-  // The `imports` array is used to import other modules.
-  // Leave empty for this example as there are no additional modules being imported.
-  imports: [],
-
   // The `controllers` array specifies the controllers to be registered within this module.
   // Controllers handle incoming HTTP requests and define the API endpoints.
-  controllers: [AppController, EventsController],
+  controllers: [EventsController],
 
   // The `providers` array defines the services that can be injected into other components.
   // Services encapsulate the business logic of the application.
-  providers: [AppService, EventsService],
+  providers: [EventsService],
 })
-export class AppModule {}
+export class EventsModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { EventsModule } from './events/events.module';
 
 // Creation New Nest Application Object
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(EventsModule);
   await app.listen(3000);
 }
 
